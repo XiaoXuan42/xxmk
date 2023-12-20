af4525d0121c85c02ff00660e081456ab279334f
SpicyNoodle <522169030@qq.com>
2023-12-20 10:54:57 +0800

    re-add parserlib

---
+++ parserlib/ast_test.go
@@ -0,0 +1,47 @@
+package parserlib
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"google.golang.org/protobuf/proto"
+)
+
+func TestAstProtoMarshalWithoutErr(t *testing.T) {
+	start := Pos{}
+	end := Pos{}
+	end.ConsumeStr("hello world\n")
+	ast := Ast{
+		Root: AstNode{
+			Type:  &Document{},
+			Start: start,
+			End:   end,
+		},
+	}
+	buf := ast.ToProtoBuf()
+	_, err := proto.Marshal(buf)
+	assert.Equal(t, nil, err)
+}
+
+func TestAstEqual(t *testing.T) {
+	mk := `# Title1
+- item1
+- item2
+
+hello world!
+## Title2
+1. item1
+2. item2
+`
+	parser := GetHtmlMKParser()
+	ast := parser.Parse(mk)
+	astProto := ast.ToProtoBuf()
+	buf, err := proto.Marshal(astProto)
+	assert.Equal(t, nil, err)
+	astProto2 := &AstProto{}
+	err = proto.Unmarshal(buf, astProto2)
+	assert.Equal(t, nil, err)
+	ast2 := &Ast{}
+	ast2.FromProtobuf(astProto2)
+	assert.Equal(t, true, ast.Eq(ast2))
+}
